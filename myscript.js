968facc8b685f531827b044381badf8b73926f4c
Tim Walkom <timwalkom@hotmail.com>
2023-12-06 20:56:20 +1030

    Made it so the user could select the grid size

--- myscript.js
+++ myscript.js
@@ -1,19 +1,45 @@
 let container = document.querySelector('.container');
+let gridSize = 16;
+let userInput = 0;
 
-function lineOfDivs() {
+
+function addGrids() {
+const addRows = document.querySelector('.addRows');
+    if (addRows) {
+        addRows.addEventListener('click', () => {
+        userInput = window.prompt("How big do you want your grid to be? Max of 100 x 100" );
+        parseInt(userInput);
+        if (userInput) { 
+            gridSize = parseInt(userInput);
+            lineOfDivs();
+        }
+        else {
+        }   
+        });
+
+    }
+}
+
+
+
+    function lineOfDivs() {
     let x = 0;
-    while (x < 16) {
+    container.textContent = '';
+    
+    while (x < gridSize) {
         const row = document.createElement('div');
-        
         let i = 0;
-        while (i < 16){
+        
+        while (i < gridSize){
         const div = document.createElement('div')
         div.style.minHeight = '35px';
         div.style.minWidth = '70px';
         div.style.borderColor = 'black';
         div.style.borderStyle = 'dotted';
         div.style.flex = '1';
-        
+        div.addEventListener('mouseover', function() {
+        div.style.backgroundColor = 'grey';
+        })
     
         row.appendChild(div);
         i++;
@@ -23,11 +49,10 @@ function lineOfDivs() {
     }
 }
 
-
+//CSS styling 
 container.style.display = 'flex';
-//container.style.justifyContent = 'space-between';
 container.style.alignItems = 'stretch';
 container.style.flex = '1';
 
 
-lineOfDivs();
+addGrids();
