ae687453579673c87dcf2b31e7f8d7ab6d714821
Tim Walkom <timwalkom@hotmail.com>
2023-12-07 16:56:47 +1030

    Completed project, grid now changes colours

--- myscript.js
+++ myscript.js
@@ -1,7 +1,8 @@
-let container = document.querySelector('.container');
+let container = document.querySelector('#container');
 let gridSize = 16;
 let userInput = 0;
-
+let gridItemWidth = 0;
+let gridItemHeight = 0;
 
 function addGrids() {
 const addRows = document.querySelector('.addRows');
@@ -9,12 +10,17 @@ const addRows = document.querySelector('.addRows');
         addRows.addEventListener('click', () => {
         userInput = window.prompt("How big do you want your grid to be? Max of 100 x 100" );
         parseInt(userInput);
-        if (userInput) { 
+        if (userInput <= 100) { 
             gridSize = parseInt(userInput);
             lineOfDivs();
+            
         }
-        else {
+        else if (userInput > 100) {
+            container.textContent = 'To big';
         }   
+        else {
+        
+        }
         });
 
     }
@@ -25,20 +31,31 @@ const addRows = document.querySelector('.addRows');
     function lineOfDivs() {
     let x = 0;
     container.textContent = '';
+    gridItemWidth = Math.min(1200/gridSize);
+    gridItemHeight = Math.min(600/gridSize);
     
     while (x < gridSize) {
+        
         const row = document.createElement('div');
         let i = 0;
         
         while (i < gridSize){
+    
         const div = document.createElement('div')
-        div.style.minHeight = '35px';
-        div.style.minWidth = '70px';
+        div.style.minHeight = `${gridItemHeight}px`;
+        div.style.minWidth = `${gridItemWidth}px`;
         div.style.borderColor = 'black';
         div.style.borderStyle = 'dotted';
         div.style.flex = '1';
+        
+        
         div.addEventListener('mouseover', function() {
-        div.style.backgroundColor = 'grey';
+        for(let a = 0; a < 10; a++) {
+            const randomColour = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            div.style.backgroundColor = randomColour;
+        }
+        
+        
         })
     
         row.appendChild(div);
@@ -46,6 +63,7 @@ const addRows = document.querySelector('.addRows');
         }
         container.appendChild(row);
         x++; 
+        
     }
 }
 
@@ -53,6 +71,10 @@ const addRows = document.querySelector('.addRows');
 container.style.display = 'flex';
 container.style.alignItems = 'stretch';
 container.style.flex = '1';
+document.getElementById("container").style.maxWidth = '1200px';
+
+
+
 
 
 addGrids();
